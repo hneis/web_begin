131ee55a7204624a82d7922501d020a73047406c
Ivanyuta Dmitriy <ivanyuta.dmitriy@gmail.com>
2020-04-13 00:02:04 +0500

    [task1] Возвращаем список url, на которых найден запрос

--- lesson1/homework/main.go
+++ lesson1/homework/main.go
@@ -11,21 +11,20 @@ import (
 var (
 	urls = []string{
 		"https://ru.wikipedia.org/wiki/%D0%9F%D0%B5%D0%BD%D0%B5%D0%BB%D0%BE%D0%BF%D0%B0",
-		"https://www.kinopoisk.ru/film/189651/",
 		"https://www.cisco.com/c/ru_ru/index.html",
 		"https://ast.ru/authors/duglas-penelopa-986618/",
 		"https://www.velosklad.ru/",
 	}
 )
 
-func findContent(pattern string, urls []string) {
+func findContent(pattern string, urls []string) (result []string) {
 	for _, url := range urls {
 		conn, err := http.Get(url)
-		defer conn.Body.Close()
-
 		if err != nil {
 			log.Println(err.Error())
+			continue
 		}
+		defer conn.Body.Close()
 
 		data, err := ioutil.ReadAll(conn.Body)
 		if err != nil {
@@ -33,12 +32,14 @@ func findContent(pattern string, urls []string) {
 		}
 
 		if bytes.Contains(data, []byte(pattern)) {
-			fmt.Println(url)
+			result = append(result, url)
 		}
 	}
+
+	return
 }
 
 func main() {
 	pattern := "Пенелопа"
-	findContent(pattern, urls)
+	fmt.Println(findContent(pattern, urls))
 }
