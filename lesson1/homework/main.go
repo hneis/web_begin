6b1fb1b34197056bfc9b0fba555b6a5236a10138
Ivanyuta Dmitriy <ivanyuta.dmitriy@gmail.com>
2020-04-13 22:45:50 +0500

    [task2] Сохранение файла с Yandex диска

--- lesson1/homework/main.go
+++ lesson1/homework/main.go
@@ -1,45 +1,143 @@
 package main
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
+	"mime"
 	"net/http"
+	"net/url"
+	"os"
+	"path"
 )
 
 var (
-	urls = []string{
-		"https://ru.wikipedia.org/wiki/%D0%9F%D0%B5%D0%BD%D0%B5%D0%BB%D0%BE%D0%BF%D0%B0",
-		"https://www.cisco.com/c/ru_ru/index.html",
-		"https://ast.ru/authors/duglas-penelopa-986618/",
-		"https://www.velosklad.ru/",
+	YANDEX_OAUTH = os.Getenv("YANDEX_OAUTH")
+	header       = map[string][]string{
+		"Accept":        {"application/json"},
+		"Content-Type":  {"application/json"},
+		"Authorization": {"OAuth " + YANDEX_OAUTH},
 	}
 )
 
-func findContent(pattern string, urls []string) (result []string) {
-	for _, url := range urls {
-		conn, err := http.Get(url)
-		if err != nil {
-			log.Println(err.Error())
-			continue
-		}
-		defer conn.Body.Close()
+type jsonResponse struct {
+	Href      string
+	Method    string
+	Templated bool
+}
 
-		data, err := ioutil.ReadAll(conn.Body)
-		if err != nil {
-			log.Println(err.Error())
-		}
+type Download struct {
+	Destination string
+	client      http.Client
+}
 
-		if bytes.Contains(data, []byte(pattern)) {
-			result = append(result, url)
-		}
+/*
+@filename to download
+*/
+func (d Download) GetFileFromYandexDisk(filename string) error {
+	url, err := url.Parse(fmt.Sprintf("https://cloud-api.yandex.net/v1/disk/resources/download?path=/%s", filename))
+	if err != nil {
+		log.Println(err)
+		return err
 	}
 
-	return
+	jsResp, err := d.queryToDownlod(url)
+	if err != nil {
+		return err
+	}
+
+	url, err = url.Parse(jsResp.Href)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = d.download(jsResp.Method, url)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d Download) queryToDownlod(url *url.URL) (jr *jsonResponse, err error) {
+	r, err := d.client.Do(&http.Request{
+		Method: "GET",
+		URL:    url,
+		Header: header,
+	})
+
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	defer r.Body.Close()
+
+	err = json.NewDecoder(r.Body).Decode(&jr)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	return jr, nil
+}
+
+func (d Download) download(m string, url *url.URL) error {
+	r, err := d.client.Do(&http.Request{
+		Method: m,
+		URL:    url,
+		Header: header,
+	})
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer r.Body.Close()
+
+	cd := r.Header.Get("Content-Disposition")
+	if cd == "" {
+		return fmt.Errorf("Content-Disposition is empty")
+	}
+
+	_, params, err := mime.ParseMediaType(cd)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	filename := params["filename"]
+	if filename == "" {
+		return fmt.Errorf("filename is empty")
+	}
+
+	o, err := os.Create(path.Join(d.Destination, filename))
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(o, r.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
-	pattern := "Пенелопа"
-	fmt.Println(findContent(pattern, urls))
+	if YANDEX_OAUTH == "" {
+		fmt.Println("Variable YANDEX_OAUTH is not pressed")
+		return
+	}
+	downloader := Download{
+		Destination: "/tmp/",
+	}
+	err := downloader.GetFileFromYandexDisk("test.jpg")
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Println("File test.jpg saved in " + downloader.Destination)
 }
