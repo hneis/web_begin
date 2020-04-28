078b5716a0a8a1a242217c5148919b12e9819723
Ivanyuta <ivanyuta.dmitriy@gmail.com>
2020-04-28 22:52:29 +0500

    [lesson5] init

---
+++ lesson5/homework/go.mod
@@ -0,0 +1,10 @@
+module github.com/hneis/web_begin/lesson5/homework
+
+go 1.14
+
+require (
+	github.com/go-chi/chi v4.1.1+incompatible
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/satori/go.uuid v1.2.0
+	github.com/sirupsen/logrus v1.5.0
+)
---
+++ lesson5/homework/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"os"
+	"os/signal"
+
+	"github.com/hneis/web_begin/lesson5/homework/server"
+	"github.com/sirupsen/logrus"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func newLogger() *logrus.Logger {
+	lg := logrus.New()
+	lg.SetReportCaller(false)
+	lg.SetFormatter(&logrus.TextFormatter{})
+	lg.SetLevel(logrus.DebugLevel)
+
+	return lg
+}
+
+func main() {
+	flagRootDir := flag.String("rootdir", "./www", "root dir of the server")
+	flagServAddr := flag.String("addr", "localhost:8888", "server address")
+	flag.Parse()
+
+	lg := newLogger()
+	db, err := sql.Open("mysql", "root:root@tcp(172.22.0.2)/blog")
+	if err != nil {
+		lg.WithError(err).Fatal("can't connect to db")
+	}
+	defer db.Close()
+	serv := server.New(lg, *flagRootDir, db)
+
+	go func() {
+		err := serv.Start(*flagServAddr)
+		if err != nil {
+			lg.WithError(err).Fatal("can't run the server")
+		}
+	}()
+
+	stopSig := make(chan os.Signal)
+	signal.Notify(stopSig, os.Interrupt, os.Kill)
+	<-stopSig
+
+}
