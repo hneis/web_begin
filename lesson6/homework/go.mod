0ff753146be67047cb8bca4ab6b8f01da0ef0ddf
Ivanyuta Dmitriy <ivanyuta.dmitriy@gmail.com>
2020-05-10 13:13:28 +0500

    [lesson6] блог на mongo

---
+++ lesson6/homework/go.mod
@@ -0,0 +1,13 @@
+module github.com/hneis/web_begin/lesson6/homework
+
+go 1.14
+
+require (
+	github.com/go-chi/chi v4.1.1+incompatible
+	github.com/gofrs/uuid v3.3.0+incompatible
+	github.com/sirupsen/logrus v1.6.0
+	github.com/volatiletech/inflect v0.0.1 // indirect
+	github.com/volatiletech/sqlboiler v3.7.1+incompatible
+	go.mongodb.org/mongo-driver v1.3.3
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+)
---
+++ lesson6/homework/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/hneis/web_begin/lesson6/homework/server"
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func newLogger() *logrus.Logger {
+	lg := logrus.New()
+	lg.SetReportCaller(false)
+	lg.SetFormatter(&logrus.TextFormatter{})
+	lg.SetLevel(logrus.DebugLevel)
+
+	return lg
+}
+
+func main() {
+	flagRootDir := flag.String("rootdir", "./www", "root dir of the server")
+	flagServAddr := flag.String("addr", "localhost:8888", "server address")
+	flag.Parse()
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	db := client.Database("blog")
+	lg := newLogger()
+	// db, err := sql.Open("mysql", "root:root@tcp(172.22.0.2)/blog")
+	// if err != nil {
+	// 	lg.WithError(err).Fatal("can't connect to db")
+	// }
+	// defer db.Close()
+	serv := server.New(lg, *flagRootDir, db, ctx)
+
+	go func() {
+		err := serv.Start(*flagServAddr)
+		if err != nil {
+			lg.WithError(err).Fatal("can't run the server")
+		}
+	}()
+
+	stopSig := make(chan os.Signal)
+	signal.Notify(stopSig, os.Interrupt, os.Kill)
+	<-stopSig
+
+}
